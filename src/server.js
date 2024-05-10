a7afed2eb3e494f3865ff4fa502540f369b1012e
Leenah Iqbal <leenah.iqbal@bbc.co.uk>
2024-05-10 09:24:27 +0100

    Add function to replace subtemplates in the page withn correct content

--- src/server.js
+++ src/server.js
@@ -14,49 +14,55 @@ const capitiliseTitleOfPage = (service) =>
 
 const findSubTemplates = (template) => {
   //wait for some magic to happen
-  // regex
-  return ["nav", "copyright"];
+  const regexp = /{% *([a-z\/]+) *%}/g;
+  const findAllSubTemplate = [...template.matchAll(regexp)];
+  const listOfSubTemplate = findAllSubTemplate.map((item) => item[1]);
+  // console.log("listOfSubTemplate", listOfSubTemplate);
+  return listOfSubTemplate;
 };
 
 const replaceProps = (currentTemplate, props = {}) => {
-  const replacer = (updatedTemplate, property) =>
-    updatedTemplate.replace(`{{${property}}}`, props[property]);
-
+  const replacer = (updatedTemplate, property) => {
+    return updatedTemplate.replace(`{{${property}}}`, props[property]);
+  };
   return Object.keys(props).reduce(replacer, currentTemplate);
 };
 
 const replaceSubTemplates = (currentTemplate, subTemplates = []) => {
-  return currentTemplate;
+  const replacer = (updatedTemplate, subTemplate) => {
+    return updatedTemplate.replace(
+      `{%${subTemplate}%}`,
+      loadPage(`/${subTemplate}`)
+    );
+  };
+
+  return subTemplates.reduce(replacer, currentTemplate);
 };
 
 const loadTemplate = (template, props = {}) => {
   const initialValue = loadPage(template);
   const subTemplates = findSubTemplates(initialValue);
-  console.log(subTemplates);
-  const templateWithPropsReplaced = replaceProps(props, initialValue);
+  const templateWithPropsReplaced = replaceProps(initialValue, props);
   const templateWithSubTemplateReplaced = replaceSubTemplates(
-    subTemplates,
-    templateWithPropsReplaced
+    templateWithPropsReplaced,
+    subTemplates
   );
+
   return templateWithSubTemplateReplaced;
 };
 
 const getPage = (service, res) => {
   res.statusCode = 200;
   const main = loadTemplate(service, { date: "12-12-22" });
-
-  // const subNav = service.includes("news") ? loadTemplate("/nav") : "";
-  // const nav = loadTemplate("./nav");
   const pageContent = loadTemplate("/template", {
     main,
     title: capitiliseTitleOfPage(service),
   });
-  // console.log(3, pageContent);
+
   res.end(pageContent);
 };
 
 const isPageExist = (url) => {
-  // look in pages folder if matching file exist
   return fs.existsSync(`${__dirname}/pages${url}.html`);
 };
 
@@ -73,5 +79,3 @@ const defaultPage = (req, res) => {
 const server = http.createServer(defaultPage);
 
 module.exports = { server };
-// ToDo
-// Make sub menu to news
